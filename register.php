04fb5c36d2ef9bc4ddb572e1fc387253e9a6ce3e
unknown <lenguyenthaikhang700@gmail.com>
2022-11-17 14:52:49 +0700

    firstcommit

---
+++ register.php
@@ -0,0 +1,113 @@
+
+<?php
+$connect = mysqli_connect('localhost', 'root', '', 'user');
+mysqli_set_charset($connect, "utf8");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <style>
+
+        .login {
+            font-size: 50px;
+            font-weight: bold;
+
+        }
+
+        .login_form {
+            width: 300px;
+            height: 450px;
+            border: 2px solid black;
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center;
+        }
+
+        .form-control {
+            height: 40px;
+            margin-top: 20px;
+            border: none;
+            box-shadow: 2px 2px black;
+        }
+
+        .btn {
+            width: 100px;
+            height: 35px;
+            
+            font-size: 20px;
+            border: none;
+            border-radius: 5px;
+            transition: 0.3s;
+
+        }
+
+        .btn:hover {
+            box-shadow: 2px 2px black;
+            background: #a39d9d;
+        }
+        .btn-register{
+            text-decoration: none;
+            margin-top: 20px !important;
+            color: black;
+        }
+        .link{
+            text-decoration: none;
+        }
+    </style>
+</head>
+
+<body>
+
+    <form action="" method="post">
+        <p>PHP Thuần </p>
+
+        <div class="login_form">
+            <div class="login">
+                <h3>Register</h3>
+            </div>
+
+            <div class="">
+                <div class="">
+                    <div class="">
+                        <input type="text" class="form-control" name="user_name_lg" placeholder="Nhập tên đăng nhập...">
+                    </div>
+                    <div class="form-group">
+
+                        <input type="password" class="form-control" name="passlg" placeholder="Nhập mật khẩu..." required>
+                    </div>
+                    <br>
+                    <?php
+                    // if (isset($_POST["dangnhap"])) {
+                    //     $tk = $_POST["user_name_lg"];
+                    //     $mk = $_POST["passlg"];
+                    //     $rows = mysqli_query($connect, "
+				    //         select * from login where username = '$tk' and password = '$mk'");
+                    //     $count = mysqli_num_rows($rows);
+                    //     if ($count == 1) {
+                    //         echo "<script type='text/javascript'>alert('Đăng nhập thành công!');</script>";
+                    //         setcookie("username",$tk);
+                    //         setcookie("password",$mk);
+                    //     } else {
+                    //         echo "<h style='color:red;'>" . "Mật khẩu hoặc tên đăng nhập ko đúng". "</h>";
+                    //         // echo "<script type='text/javascript'>alert('mật khẩu hoặc tên đăng nhập ko đúng');</script>";
+                    //     }
+                    // }
+                    ?>
+                    <br>
+
+                    <button type="submit" class="btn " name="dangnhap">Login</button>
+                        <div class="btn-register">
+                    <a  class="link" href="login.php">Login</a>
+                </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
